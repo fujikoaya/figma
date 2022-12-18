29efcf8dc4ff6697742acca1429c39880c4da9fa
apple <wonderfulaya.2020@gmail.com>
2022-12-17 18:32:10 -0800

    no message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(function(){
-    // Petlivers
-	$(window).on('load scroll',function (){
-		$('.petlivers').each(function(){
+$(function () {
+	// Petlivers
+	$(window).on('load scroll', function () {
+		$('.petlivers').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -9,31 +9,49 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
 	});
-    // About
-    $(window).on('load scroll',function (){
-		$('.about').each(function(){
-			//ターゲットの位置を取得
-			var target = $(this).offset().top;
-			//スクロール量を取得
-			var scroll = $(window).scrollTop();
-			//ウィンドウの高さを取得
-			var height = $(window).height();
-			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
-				//クラスを付与
-				$(this).addClass('active');
-			}
+	// About
+	//  $(window).on('load scroll',function (){
+	//  	$('.about_1').each(function(){
+	//  		//ターゲットの位置を取得
+	//  		var target = $(this).offset().top;
+	//  		//スクロール量を取得
+	//  		var scroll = $(window).scrollTop();
+	//  		//ウィンドウの高さを取得
+	//  		var height = $(window).height();
+	//  		//ターゲットまでスクロールするとフェードインする
+	//  		if (scroll > target - height){
+	//  			//クラスを付与
+	//  			$(this).addClass('active');
+	//  		}
+	//  	});
+	//  });
+	$(window).on('load scroll', function(){
+		//現時点のスクロールの高さ取得
+		var scrollPosition = $(window).scrollTop();
+		//ウィンドウの高さ取得
+		var windowHeight = $(window).height();
+	 
+		$('.about-a').each(function(){
+		  //要素の位置（高さ）を取得
+		  var elemPosition = $(this).offset().top;
+		  //スクロールの高さが要素の位置を超えたら以下のスタイルを適用
+		  if(elemPosition < scrollPosition + windowHeight){
+			$(this).css({
+			  opacity: 1,
+			  transform: 'translateY(0)'
+			});
+		  }
 		});
-	});
-    // Sponser
-    $(window).on('load scroll',function (){
-		$('.sponser').each(function(){
+	  });
+	// Sponser
+	$(window).on('load scroll', function () {
+		$('.sponser').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -41,7 +59,7 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
@@ -49,10 +67,10 @@ $(function(){
 	});
 });
 
-$(function(){
-    // service
-	$(window).on('load scroll',function (){
-		$('.customer').each(function(){
+$(function () {
+	// service
+	$(window).on('load scroll', function () {
+		$('.customer').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -60,15 +78,15 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
 	});
-    // customer
-	$(window).on('load scroll',function (){
-		$('.service').each(function(){
+	// customer
+	$(window).on('load scroll', function () {
+		$('.service').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -76,16 +94,16 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
-		
+
 	});
-    // contact
-	$(window).on('load scroll',function (){
-		$('.ct-a').each(function(){
+	// contact
+	$(window).on('load scroll', function () {
+		$('.ct-a').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -93,16 +111,16 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
-		
+
 	});
-    // company
-	$(window).on('load scroll',function (){
-		$('.lw-m').each(function(){
+	// company
+	$(window).on('load scroll', function () {
+		$('.lw-m').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -110,15 +128,15 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
-		
+
 	});
-	$(window).on('load scroll',function (){
-		$('.cv-i ').each(function(){
+	$(window).on('load scroll', function () {
+		$('.cv-i ').each(function () {
 			//ターゲットの位置を取得
 			var target = $(this).offset().top;
 			//スクロール量を取得
@@ -126,11 +144,11 @@ $(function(){
 			//ウィンドウの高さを取得
 			var height = $(window).height();
 			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
+			if (scroll > target - height) {
 				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
-		
+
 	});
 });
